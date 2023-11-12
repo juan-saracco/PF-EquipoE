dd7dca7d037895c65b1a8ae4018de385be187570
RJR <rodrigoribetti@gmail.com>
2023-11-12 20:32:50 -0300

    [ADD]Agredado de interfases repositorios

---
+++ demo/demo/src/main/java/com/proyectoFinal/demo/repositorios/UsuarioRepositorio.java
@@ -0,0 +1,10 @@
+package com.proyectoFinal.demo.repositorios;
+
+import com.proyectoFinal.demo.entidades.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepositorio extends JpaRepository<Usuario, String> {
+
+}
