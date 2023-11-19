efb26c2c33b348cb3703494a98594a956c3b03c2
RJR <rodrigoribetti@gmail.com>
2023-11-19 12:05:50 -0300

    Pedido Corregido

---
+++ demo/demo/src/main/java/com/proyectoFinal/demo/entidades/Pedido.java
@@ -0,0 +1,107 @@
+
+package com.proyectoFinal.demo.entidades;
+
+import java.util.Date;
+import javax.persistence.*;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Pedido {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    @OneToOne
+    private Usuario consumidor;
+    @OneToOne
+    private Proveedor proveedor;
+    private String solicitud;
+    private Boolean estadoPedido;
+    private Double cotizacion;
+
+    @OneToOne
+    private FeedBack feedBack;
+    @Temporal(TemporalType.DATE)
+    private Date alta;
+
+    @Temporal(TemporalType.DATE)
+    private Date fecha_modificacion;
+
+    public Pedido() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Usuario getConsumidor() {
+        return consumidor;
+    }
+
+    public void setConsumidor(Usuario consumidor) {
+        this.consumidor = consumidor;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public String getSolicitud() {
+        return solicitud;
+    }
+
+    public void setSolicitud(String solicitud) {
+        this.solicitud = solicitud;
+    }
+
+    public Boolean getEstadoPedido() {
+        return estadoPedido;
+    }
+
+    public void setEstadoPedido(Boolean estadoPedido) {
+        this.estadoPedido = estadoPedido;
+    }
+
+    public Double getCotizacion() {
+        return cotizacion;
+    }
+
+    public void setCotizacion(Double cotizacion) {
+        this.cotizacion = cotizacion;
+    }
+
+    public FeedBack getFeedBack() {
+        return feedBack;
+    }
+
+    public void setFeedBack(FeedBack feedBack) {
+        this.feedBack = feedBack;
+    }
+
+    public Date getAlta() {
+        return alta;
+    }
+
+    public void setAlta(Date alta) {
+        this.alta = alta;
+    }
+
+    public Date getFecha_modificacion() {
+        return fecha_modificacion;
+    }
+
+    public void setFecha_modificacion(Date fecha_modificacion) {
+        this.fecha_modificacion = fecha_modificacion;
+    }
+}
