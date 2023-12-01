2f092a61555b2bbecf88bff20d7be752db52935a
Ester Canciano <m.ester.can@gmail.com>
2023-12-01 10:40:12 -0300

    [upd] tablero de busqueda

--- demo/demo/src/main/java/com/proyectoFinal/demo/controladores/proveedorControlador.java
+++ demo/demo/src/main/java/com/proyectoFinal/demo/controladores/proveedorControlador.java
@@ -92,7 +92,7 @@ public class proveedorControlador {
         modelo.addAttribute("oficios", oficios);
         
         if (error != null) {
-            modelo.put("Error", error);
+            modelo.put("Error", "No se pudo modificar el oficio");
         }
         return "editarOficioProv.html";
     }
